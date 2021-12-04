737a83a95d6c572f44525c2b8444baeff4d2df77
CoffeESIME <heromfabian@gmail.com>
2021-12-03 18:00:24 -0600

    merge with EventHandler

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './CreateTodoButton.css'
+function CreateTodoButton(){
+
+    const onClickButton=(msg)=>{
+        alert(msg);
+    }
+    return(
+        <button 
+        className='CreateTodoButton'
+        onClick={()=>onClickButton("aparecera modal")}
+        >
+            +
+        </button>
+    );
+}
+
+export {CreateTodoButton};
---
+++ src/TodoItem/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './TodoItem.css'
+function TodoItem (props){
+    let class1 = 'Icon Icon-check';
+    let class2 = 'TodoItem-p';
+    if (props.completed===true) {
+      class1 += ' Icon-check--active';
+      class2 += ' TodoItem-p--complete'
+    }
+
+    return (
+       
+        <li className='TodoItem'>
+            <span 
+            className={class1}
+            onClick={props.onComplete}
+            > ✓</span>
+            <p  className={class2}>{props.text}</p>
+            <span  
+            className="Icon Icon-delete"
+            onClick={props.onDelete}
+            >X</span>
+        </li>
+    );
+}
+
+export {TodoItem};
