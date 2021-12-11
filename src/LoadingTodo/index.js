de199271531eeedc22c93e1a55c58b7dca1ee795
CoffeESIME <heromfabian@gmail.com>
2021-12-10 18:46:06 -0600

    some screenshots

--- src/LoadingTodo/index.js
+++ src/LoadingTodo/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 function LoadingTodo(){
     return(
-        <div class="flex items-center justify-center space-x-2 animate-pulse py-4">
-        <div class="w-24 h-24 bg-yellow-800 rounded-full"></div>
-        <div class="w-24 h-24 bg-yellow-800 rounded-full"></div>
-        <div class="w-24 h-24 bg-yellow-800 rounded-full"></div>
+        <div className="flex items-center justify-center space-x-2 animate-pulse py-4">
+        <div className="w-24 h-24 bg-yellow-800 rounded-full"></div>
+        <div className="w-24 h-24 bg-yellow-800 rounded-full"></div>
+        <div className="w-24 h-24 bg-yellow-800 rounded-full"></div>
     </div>
     
     );
